98ec44f6b36cdbe221a5d52e7fbc21a4a33ca4ec
Philip Ray Bontia <bphilipray@gmail.com>
2025-12-31 11:13:57 +0800

    Migrate app package from task_matrix to quadrant
    - Update package name: com.bphil.task_matrix → com.bphil.quadrant
    - Update app name in pubspec.yaml: task_matrix → quadrant
    - Move MainActivity to new package location
    - Update build.gradle.kts with new namespace and applicationId
    - Update Firebase google-services.json with new package configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ java {
 }
 
 android {
-    namespace = "com.bphil.task_matrix"
+    namespace = "com.bphil.quadrant"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.bphil.task_matrix"
+        applicationId = "com.bphil.quadrant"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/bphil/quadrant/MainActivity.kt
+++ android/app/src/main/kotlin/com/bphil/quadrant/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bphil.task_matrix
+package com.bphil.quadrant
 
 import io.flutter.embedding.android.FlutterActivity
 
